826c3b9049d50cc425a9160926cabca3044dea67
Ekateryna Gruvman <kate.gruvman@ukr.net>
2021-02-20 17:48:29 +0200

    Update task-4.js

--- task-4.js
+++ task-4.js
@@ -1,13 +1,25 @@
+
+
 class User {
-    constructor({ login, email, password, status }) {
-        this.login = login;
+
+    constructor({ login, emeil, password, status }) {
+        this._login = login;
         this.emeil = emeil;
         this.password = password;
         this.status = status;
     }
+    showUsers() {
+        console.log(this.users)
+    }
     showLogin(login) {
-        console.log(login)
-     }
+        return this._login;
+    }
+    get login() {
+        return this._login
+    }
+    addUser() {
+        
+    }
 }
 
 const bob = new User({
@@ -16,5 +28,25 @@ const bob = new User({
     password: 'jjkjkjkjkjkj',
     status: 'visitor',
 });
-console.log(bob)
+// console.log(bob)
+// console.log(bob.showLogin())
 console.log(bob.login)
+console.log(bob.showUser)
+
+
+class Admin extends User{
+    constructor({masterPassword,  ...rest }) {
+        super(rest);
+        this.masterPassword = masterPassword;   
+    }
+
+}
+const bobAdmin = new Admin(
+    {
+    login: 'bobAdmin',
+    emeil: 'bob@MediaList.com',
+    password: 'jjkjsdkjkjk',
+    status: 'admin',
+    masterPassword: 'sjkajskajsk'
+})
+    console.log(bobAdmin.login)
