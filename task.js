efea874be8d309aa7351318d89a67306b5c477c2
Ekateryna Gruvman <kate.gruvman@ukr.net>
2021-03-06 17:07:27 +0200

    tas

---
+++ task.js
@@ -0,0 +1,19 @@
+'use strict'
+function Book() {
+    this.name = 'foo'
+}
+
+Book.prototype = {
+    getName: function() {
+        return this.name;
+    }
+};
+
+const book = new Book();
+
+Book.prototype.getUpperName = function() {
+    return this.getName().toUpperCase();
+}
+
+book.getUpperName();
+
